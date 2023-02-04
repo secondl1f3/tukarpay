8ebb5a0040897f2cf14a2b5aac01b0b9faed80df
secondl1f3 <ahmad.fajar@outlook.co.id>
2023-02-04 23:52:32 +0300

    initialize the project

---
+++ src/main/java/com/fanabaka/tukarpay/TukarpayApplication.java
@@ -0,0 +1,13 @@
+package com.fanabaka.tukarpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukarpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TukarpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fanabaka/tukarpay/TukarpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanabaka.tukarpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukarpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
